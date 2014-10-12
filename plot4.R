ed4c6bd007fd6e08b3b0cc92934ecb77e0ee3355
rhatol <rhatol@yahoo.com>
2014-10-13 06:11:34 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -7,7 +7,7 @@ PROJ1$DateTime <- DateTime
 
 ## initialize device row/col
 par(mfrow = c(2,2))
-par(mar =c(4,4,4,4))
+par(mar =c(4,4,2,2))
 
 ## do the plots
 plot(PROJ1$DateTime,PROJ1$Global_active_power, type="l",ylab="Global Active Power", xlab="")
