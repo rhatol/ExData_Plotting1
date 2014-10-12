0094273303c3634d746814526b8a43157a660a57
rhatol <rhatol@yahoo.com>
2014-10-13 03:10:37 +0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,26 @@
+## read file that is already filtered feb 1-2, 2007
+PROJ1 <- read.table("d:\\household_power_consumption_2-1-07_to_2-2-07.txt", header=TRUE, sep=";", as.is = FALSE, dec=".", stringsAsFactors=FALSE,colClasses=c("character","character",rep("numeric",7)))
+
+## covert Date & Time variable to a single DateTime column
+DateTime <- strptime(paste(PROJ1$Date,PROJ1$Time),"%d/%m/%Y %H:%M:%S")
+PROJ1$DateTime <- DateTime
+
+## initialize device row/col
+par(mfrow = c(2,2))
+
+## do the plots
+plot(stagePROJ1$DateTime,stagePROJ1$Global_active_power, type="l",ylab="Global Active Power", xlab="")
+
+plot(stagePROJ1$DateTime,stagePROJ1$Voltage, type="l",ylab="Voltage",xlab="datetime")
+
+plot(stagePROJ1$DateTime,stagePROJ1$Sub_metering_1, type="n",ylab="Energy sub metering", xlab="")
+points(stagePROJ1$DateTime,stagePROJ1$Sub_metering_1,col="black",type="l")
+points(stagePROJ1$DateTime,stagePROJ1$Sub_metering_2,col="red",type="l")
+points(stagePROJ1$DateTime,stagePROJ1$Sub_metering_3,col="blue",type="l")
+legend("topright",col=c("black","red","blue"),legend =c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch="-")
+
+plot(stagePROJ1$DateTime,stagePROJ1$Global_reactive_power, type="l",ylab="Globe_reactive_power",xlab="datetime")
+
+#copy to device png
+dev.copy(png,file="plot3.png")
+dev.off()
